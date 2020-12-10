2a25fbfe8f36eff42efad7ed033f48cf70e24a81
HH <huang_hao_zi@163.com>
2020-12-10 21:22:08 +0800

    Update push.py

--- push.py
+++ push.py
@@ -3,14 +3,16 @@ import json
 import os
 
 token = os.getenv('token')
-title = '选股器今日数据'  # 改成你要的标题内容
+title = '选股器今日数据'
+topic = 'StockChoser'
 f = open('推送.html', 'r', encoding='utf-8')
 content = f.read()
 url = 'http://pushplus.hxtrip.com/send'
 data = {
     "token": token,
     "title": title,
-    "content": content
+    "content": content,
+    "topic": topic
 }
 body = json.dumps(data).encode(encoding='utf-8')
 headers = {'Content-Type': 'application/json'}
